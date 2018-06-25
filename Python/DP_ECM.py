505e671a2c477bc3d95697f09192b531c4fec4bd
Raja Selvakumar <rs4895@gmail.com>
2018-06-25 13:08:38 -0700

    py init

--- Python/DP_ECM.py
+++ Python/DP_ECM.py
@@ -5,4 +5,16 @@ Created on Mon Jun 25 11:57:24 2018
 @author: rselvak6
 """
 
-sdfsdf
+import matplotlib
+import os
+
+Rc = 1.94 #K/W
+Ru = 3.08
+Cc = 62.7 #J/K
+Cs = 4.5
+
+SOC_o = 0.5
+T_inf = 25
+V_oc = 3.3
+C_1 = 2500
+C_2 = 
