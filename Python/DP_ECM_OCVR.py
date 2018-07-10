58626bd80db0c5ec2a2a1f3d09d17211f6b0f74a
Raja Selvakumar <rs4895@gmail.com>
2018-07-09 18:46:56 -0700

    updated OCVR

--- Python/DP_ECM_OCVR.py
+++ Python/DP_ECM_OCVR.py
@@ -32,52 +32,74 @@ V_min = 2 #V
 V_max = 3.6
 SOC_min = 0.25
 SOC_max = 0.75
+C_batt = 2.3*3600;
 
 #Voc from file
-Voc = np.fromfile('Voc.dat',dtype=float)
+V_oc = np.loadtxt('/home/rselvak6/Documents/LGChem/Python/Voc.dat',delimiter=',',dtype=float)
 
 #free parameters
 t_max = 1800
 dt = 1
-
-#grids
-SOC_grid = np.arange(SOC_min,SOC_max,0.005)
-
-num_states = len(SOC_grid)
 N = t_max-t_0
+#%%Playground
+test= [V_oc[len(V_oc[:,0])*x,1] for x in V_oc[:,0] if x==0.25][0]
+
+#%% Preallocate grid space
+#state grids
+SOC_grid = np.arange(SOC_min,SOC_max,0.005)
+num_states = len(SOC_grid)
 
 #control and utility 
-I_opt = np.zeros((num_states,N))
 V = np.ones((num_states,N+1))*math.inf
+I_opt = np.zeros((num_states,N))
 
 #terminal conditions
 SOC_f = SOC_max
-V[:,N+1] = 0
-
+V[:,N] = 0
+#%% DP
 def DP():
     start = tm.time()
-    for i in range(t_max,t_0-1,-dt):
-        for j in range(0,num_states):
-            lb = max(I_min, C_batt/dt*(SOC_grid[j]-SOC_max))
-            ub = min(I_max, C_batt/dt*(SOC_grid[j]-SOC_min))
-            if i==t_max:
-                ub = min(I_max, C_batt/dt*(SOC_grid[j]-SOC_max))  
-            I_opt_grid = np.linspace(lb,ub,100)
-            C_k = 1 #coefficient of input term            
+    for k in range(t_max,t_0-1,-dt):
+        for idx in range(0,num_states):
             
-            I_opt[-i,j] = (ub-lb)/2;   
+            #lower/upper control bounds
+            v_oc = [V_oc[len(V_oc[:,0])*x,1] for x in V_oc[:,0] if x==0.25][0]
+            lb = max(I_min, C_batt/dt*(SOC_grid[idx]-SOC_max), (V_min-v_oc)/R_0)
+            ub = min(I_max, C_batt/dt*(SOC_grid[idx]-SOC_min), (V_max-v_oc)/R_0)
+            if k==t_max:
+                ub = min(I_max, C_batt/dt*(SOC_grid[idx]-SOC_f), (V_max-v_oc)/R_0) 
+                
+            #control initialization 
+            I_grid = np.linspace(lb,ub,100)
+            
+            #value function
+            c_k = I_grid 
+            
+            #iterate next SOC
+            SOC_nxt = SOC_grid[idx] + I_grid/C_batt*dt          
+            
+            #value function interpolation
+            V_nxt = np.interp(SOC_grid,V[:,k+1],SOC_nxt)
+            
+            #Bellman
+            [V[idx,k],ind] = max(c_k+V_nxt)
+            
+            #save optimal control
+            I_opt[idx,k] = I_grid[ind]
     stop = tm.time()
     print('DP Time:', '%.2f'%(stop-start),'s')
     return I_opt
-    
-def sim(I_sim):
-    SOC_sim = np.zeros((N,num_states))
-    SOC_sim[t_0,:] = [SOC_min]*num_states
-    for i in range(t_0+1,t_max,dt):
-        for j in range(0,num_states):
-            SOC_sim[i,j] = SOC_sim[i-1,j]+I_sim[i,j]*dt/C_batt
+#%% Simulation   
+def sim(I_opt):
+    SOC_sim = [0]*N
+    I_sim = [0]*N
+    #initial condition
+    SOC_sim[0] = SOC_min
+    for i in range(t_0,t_max-1,dt):
+        I_sim[i] = np.interp(SOC_grid,I_opt[:,i],SOC_sim[i])
+        SOC_sim[i+1] = SOC_sim[i]+I_sim[i]*dt/C_batt
     return SOC_sim
-    
+
+#%% Main 
 ret = DP()
 SOC_out = sim(ret)
-print(['%.2f'%j for j in ret[1000,:]])
