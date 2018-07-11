fdfa186d9b6bd225ca0812f5961b34826d5a5827
Raja Selvakumar <rs4895@gmail.com>
2018-07-10 19:10:53 -0700

    python 0710 end of day

--- Python/DP_ECM_OCVR.py
+++ Python/DP_ECM_OCVR.py
@@ -20,7 +20,6 @@ T_inf = 25 #C
 t_0 = 0 #sec
 
 #Values taken from Perez et. al 2015 based on initial conditions
-V_oc = 3.3 #V
 C_1 = 2500 #F
 C_2 = 5.5
 R_0 = 0.01 #Ohms
@@ -30,8 +29,8 @@ I_min = 0 #A
 I_max = 46
 V_min = 2 #V
 V_max = 3.6
-SOC_min = 0.25
-SOC_max = 0.75
+SOC_min = 0.1
+SOC_max = 0.9
 C_batt = 2.3*3600;
 
 #Voc from file
@@ -63,7 +62,7 @@ def DP():
         for idx in range(0,num_states):
             
             #lower/upper control bounds
-            v_oc = [V_oc[len(V_oc[:,0])*x,1] for x in V_oc[:,0] if x==0.25][0]
+            v_oc = [V_oc[len(V_oc[:,0])*x,1] for x in V_oc[:,0] if x==SOC_grid[idx]][0]
             lb = max(I_min, C_batt/dt*(SOC_grid[idx]-SOC_max), (V_min-v_oc)/R_0)
             ub = min(I_max, C_batt/dt*(SOC_grid[idx]-SOC_min), (V_max-v_oc)/R_0)
             if k==t_max-1:
